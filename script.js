7b70358172dda8284606c38842ce11f2e773a3b6
Gage <77414756+Royuin@users.noreply.github.com>
2023-04-24 17:18:13 -0500

    made functions for merge sorting array

--- script.js
+++ script.js
@@ -3,3 +3,33 @@ function nodeFactory(value) {
 }
 
 let testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+function merge(left, right) {
+  let arr = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      arr.push(left.shift());
+    } else {
+      arr.push(right.shift());
+    }
+  }
+
+  return [...arr, ...left, ...right];
+}
+
+function mergeSort(array) {
+  let uniqueArray = array.filter(onlyUnique);
+
+  const half = uniqueArray.length / 2;
+
+  if (uniqueArray.length < 2) {
+    return uniqueArray;
+  }
+
+  const left = uniqueArray.splice(0, half);
+  return merge(mergeSort(left), mergeSort(uniqueArray));
+}
+function buildTree(array) {
+  let root = array[Math.round(array.length / 2)];
+}
