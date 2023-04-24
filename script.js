c8284851e69ad4c7725484b63315a792c19d2b7f
Gage <77414756+Royuin@users.noreply.github.com>
2023-04-24 17:17:43 -0500

    made test array to test all future features added

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-const nodeFactory = function (value) {
+function nodeFactory(value) {
   return { value, left: null, right: null };
-};
+}
+
+let testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
